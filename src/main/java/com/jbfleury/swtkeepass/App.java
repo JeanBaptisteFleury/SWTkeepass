6fbc75b2c6eac1d84930b003a25f16f27b8f2983
Jean-Baptiste Fleury <jean-baptiste.fleury@epitech.eu>
2020-06-12 19:51:17 +0200

    First commit
    Just a basic HelloWorld message

---
+++ src/main/java/com/jbfleury/swtkeepass/App.java
@@ -0,0 +1,13 @@
+package com.jbfleury.swtkeepass;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
