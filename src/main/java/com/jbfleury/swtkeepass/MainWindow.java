b3e4d223cc2a1e90734880e394af580e7dc193da
Jean-Baptiste Fleury <no-reply>
2020-06-12 21:48:02 +0200

    Added a menubar and the basic menu items
    Just placeholders, no actual code has been done yet

--- src/main/java/com/jbfleury/swtkeepass/MainWindow.java
+++ src/main/java/com/jbfleury/swtkeepass/MainWindow.java
@@ -1,12 +1,16 @@
 package com.jbfleury.swtkeepass;
 
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.layout.GridLayout;
 
 public class MainWindow {
 
-	protected Shell shell;
+	protected Shell shlSwtkeepass;
 
 	/**
 	 * Launch the application.
@@ -27,9 +31,9 @@ public class MainWindow {
 	public void open() {
 		Display display = Display.getDefault();
 		createContents();
-		shell.open();
-		shell.layout();
-		while (!shell.isDisposed()) {
+		shlSwtkeepass.open();
+		shlSwtkeepass.layout();
+		while (!shlSwtkeepass.isDisposed()) {
 			if (!display.readAndDispatch()) {
 				display.sleep();
 			}
@@ -40,10 +44,34 @@ public class MainWindow {
 	 * Create contents of the window.
 	 */
 	protected void createContents() {
-		shell = new Shell();
-		shell.setSize(450, 300);
-		shell.setText("SWTKeepass");
+		shlSwtkeepass = new Shell();
+		shlSwtkeepass.setSize(450, 300);
+		shlSwtkeepass.setText("SWTKeepass");
+		shlSwtkeepass.setLayout(new GridLayout(1, false));
+		
+		Menu menu = new Menu(shlSwtkeepass, SWT.BAR);
+		shlSwtkeepass.setMenuBar(menu);
+		
+		MenuItem mntmFiles = new MenuItem(menu, SWT.CASCADE);
+		mntmFiles.setText("File");
+		
+		Menu menu_1 = new Menu(mntmFiles);
+		mntmFiles.setMenu(menu_1);
+		
+		MenuItem mntmNewItem = new MenuItem(menu_1, SWT.NONE);
+		mntmNewItem.setText("Open File...");
+		
+		MenuItem mntmNewItem_1 = new MenuItem(menu_1, SWT.NONE);
+		mntmNewItem_1.setText("Exit");
+		
+		MenuItem mntmHelp = new MenuItem(menu, SWT.CASCADE);
+		mntmHelp.setText("Help");
+		
+		Menu menu_2 = new Menu(mntmHelp);
+		mntmHelp.setMenu(menu_2);
+		
+		MenuItem mntmAboutSwtkeepass = new MenuItem(menu_2, SWT.NONE);
+		mntmAboutSwtkeepass.setText("About SWTKeepass");
 
 	}
-
 }
